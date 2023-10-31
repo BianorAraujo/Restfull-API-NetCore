11d2453381241dac686814263ef3376f066e8ce8
Bianor Araujo <bianor.araujo@gmail.com>
2023-10-31 19:57:32 -0300

    Projeto finalizado

---
+++ Project/PessoaApi/Controllers/PessoaController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PessoaApi.Entities;
+
+namespace PessoaApi.Controllers;
+
+[ApiController]
+[Route("[Controller]")]
+public class PessoaController : ControllerBase
+{
+    public static List<Pessoa> pessoas;
+
+    public PessoaController()
+    {
+        if (pessoas == null || pessoas.Count == 0)
+            pessoas = new Pessoa().CriarPessoas();
+    }
+    
+    [HttpGet]
+    public ActionResult<IEnumerable<Pessoa>> Get()
+    {
+        if (pessoas == null)
+            return NotFound();
+
+        return Ok(pessoas);
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<Pessoa> Get(int id)
+    {
+        var pessoa = pessoas.FirstOrDefault(p => p.Id == id);
+
+        if (pessoa == null)
+            return NotFound();
+
+        return Ok(pessoa);
+    }
+
+    [HttpPost]
+    public ActionResult Post([FromBody] Pessoa pessoa)
+    {
+        pessoa.Id = pessoas.Count + 1;
+        pessoas.Add(pessoa);
+
+        return CreatedAtAction(nameof(Get), new { id = pessoa.Id }, pessoa);
+    }
+
+    [HttpDelete("{id}")]
+    public ActionResult Delete(int id)
+    {
+        var pessoa = pessoas.FirstOrDefault(p => p.Id == id);
+
+        if (pessoa == null)
+            return NotFound();
+
+        pessoas.Remove(pessoa);
+
+        return NoContent();
+    }
+
+    [HttpPut("{id}")]
+    public ActionResult Put(int id, [FromBody] Pessoa pessoa)
+    {
+        var existingPessoa = pessoas.FirstOrDefault(p => p.Id == id);
+
+        if (existingPessoa == null)
+            return NotFound();
+
+        existingPessoa.Nome = pessoa.Nome;
+        existingPessoa.Idade = pessoa.Idade;
+
+        return NoContent();
+    }
+
+}
---
+++ Project/PessoaApi/Entities/Pessoa.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace PessoaApi.Entities
+{
+	public class Pessoa
+	{
+		public int Id { get; set; }
+		public string Nome { get; set; }
+		public int Idade { get; set; }
+				
+
+		public List<Pessoa> CriarPessoas()
+		{
+			var names = new string[] { "Bianor", "Ricardo", "Joao" };
+
+            List<Pessoa> pessoaList = new List<Pessoa>();
+
+
+            for (int i = 1; i <= names.Length; i++)
+			{
+                pessoaList.Add(new Pessoa() {
+                    Id = i,
+                    Nome = names[i-1],
+                    Idade = Random.Shared.Next(20, 60)
+                });
+            }
+
+            return pessoaList;
+		}
+	}
+}
+
